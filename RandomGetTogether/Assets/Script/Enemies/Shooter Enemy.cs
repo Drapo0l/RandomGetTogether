76facceca70408257b6294fddc99a7b9b669562b
ACUno90 <159036403+ACUno90@users.noreply.github.com>
2024-09-14 14:24:16 -0400

    pulling

--- RandomGetTogether/Assets/Script/Enemies/Shooter Enemy.cs
+++ RandomGetTogether/Assets/Script/Enemies/Shooter Enemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ShooterEnemy : MonoBehaviour, DamageFE
+public class ShooterEnemy : MonoBehaviour, DamageFE 
 {
     [SerializeField] Renderer Model;
 
