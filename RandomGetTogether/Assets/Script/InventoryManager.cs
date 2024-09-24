ae4002d3eb0f55419e83d882cf6fb72d5d37898b
Candace <151546391+BeenCandace@users.noreply.github.com>
2024-09-24 09:20:10 -0400

    Slot quantities for inventory added.

--- RandomGetTogether/Assets/Script/InventoryManager.cs
+++ RandomGetTogether/Assets/Script/InventoryManager.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
     [SerializeField] private GameObject slotHolder;
-    [SerializeField] private ItemScript itemToAdd;
+    [SerializeField] private ItemScript itemToAdd; 
     [SerializeField] private ItemScript itemToRemove;
 
+    
    public List<ItemScript> items = new List<ItemScript>();
 
     private GameObject[] slots;
@@ -15,9 +17,35 @@ public class InventoryManager : MonoBehaviour
     public void Start()
     {
         slots = new GameObject[slotHolder.transform.childCount];
+        for (int i = 0;i < slotHolder.transform.childCount; i++)
+        slots[i] = slotHolder.transform.GetChild(i).gameObject;
+
+        RefreshUI();
+
         Add(itemToAdd);
         Remove(itemToRemove);
     }
+
+    public void RefreshUI()
+    { 
+        for (int i = 0; i < slots.Length; i++) 
+        {
+            try
+            {
+                slots[i].transform.GetChild(0).GetChild(0).GetComponent<Image>().enabled = true;
+                slots[i].transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = items[i].ItemImage;
+            }
+
+            catch
+            {
+                slots[i].transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = null;
+                slots[i].transform.GetChild(0).GetChild(0).GetComponent<Image>().enabled = false;
+
+            }
+        }
+    
+    }
+
 
     public void Add(ItemScript item)
     { 
